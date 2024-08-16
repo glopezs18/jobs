ceb0d24a23af8902f1ac15bdb6054067aa3fb2c1
glopezs18 <glopezs18@miumg.edu.gt>
2024-08-16 17:59:51 -0600

    changes activity

--- src/app/client/activity/a-detail/a-detail.page.ts
+++ src/app/client/activity/a-detail/a-detail.page.ts
@@ -1,7 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton, IonRow, IonCol, IonLabel, IonIcon, IonThumbnail, IonItem, IonText, IonList, IonBadge } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { 
+  personCircle,
+  locationOutline,
+  readerOutline
+} from 'ionicons/icons';
 import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
@@ -9,14 +15,16 @@ import { ActivatedRoute, Router } from "@angular/router";
   templateUrl: './a-detail.page.html',
   styleUrls: ['./a-detail.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonBackButton]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonBackButton, IonRow, IonCol, IonLabel, IonIcon, IonThumbnail, IonItem, IonText, IonList, IonBadge]
 })
 export class ADetailPage implements OnInit {
 
   current_activity_id: any = null;
   constructor(
     private route: ActivatedRoute
-  ) { }
+  ) { 
+    addIcons({ personCircle, locationOutline, readerOutline })
+  }
 
   ngOnInit() {
     this.current_activity_id = this.route.snapshot.paramMap.get("id");
