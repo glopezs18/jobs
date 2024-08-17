4afcdaeed7c76eb255f780269bcbb3cd9518dcec
glopezs18 <glopezs18@miumg.edu.gt>
2024-08-16 23:11:01 -0600

    add home elements

--- src/app/client/activity/activity.page.ts
+++ src/app/client/activity/activity.page.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonListHeader, IonThumbnail, IonIcon, IonLabel, IonSkeletonText, IonButton } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../../explore-container/explore-container.component';
 import { addIcons } from 'ionicons';
-import { musicalNotes, pizza, school, storefront } from 'ionicons/icons';
+import { musicalNotes, pizza, school, storefront, hammerOutline, libraryOutline, pawOutline, constructOutline } from 'ionicons/icons';
 import {RouterModule} from '@angular/router';
 
 @Component({
@@ -19,39 +19,43 @@ export class ActivityPage implements OnInit {
   recent_activity = [
     {
       id: 'a1',
-      activity: 'Majadas Once',
+      activity: 'Instalación de cableado',
       date: '27 jul . 10:52pm',
-      cost: 'GTQ33.98',
-      icon: 'pizza'
+      cost: 'GTQ500.08',
+      icon: 'hammer-outline'
     },
     {
       id: 'a2',
-      activity: 'Miraflores',
+      activity: 'Clases de refuerzo',
       date: '27 jul . 7:52pm',
       cost: 'GTQ40.37',
-      icon: 'musical-notes'
+      icon: 'library-outline'
     },
     {
       id: 'a3',
-      activity: 'Metamercado',
+      activity: 'Consulta de veterinario',
       date: '6 jul . 6:28pm',
       cost: 'GTQ41.92',
-      icon: 'school'
+      icon: 'paw-outline'
     },
     {
       id: 'a4',
-      activity: 'Rincon del steak',
+      activity: 'Reparación de puerta',
       date: '16 jun . 6:23pm',
-      cost: 'GTQ72.55',
-      icon: 'storefront'
+      cost: 'GTQ350.00',
+      icon: 'construct-outline'
     }
   ]
 
   constructor() {
-    addIcons({ musicalNotes, pizza, school, storefront });
+    addIcons({ musicalNotes, pizza, school, storefront, hammerOutline, libraryOutline, pawOutline, constructOutline });
    }
 
   ngOnInit() {
+
+    if (this.recent_activity.length > 0) {
+      this.loaded = true;
+    }
   }
 
 
--- src/app/client/home/home.page.ts
+++ src/app/client/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonCol, IonRow, IonImg, IonItem, IonLabel, IonGrid, IonButton, IonThumbnail, IonSkeletonText, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCardSubtitle } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../../explore-container/explore-container.component';
 
 @Component({
@@ -7,13 +8,60 @@ import { ExploreContainerComponent } from '../../explore-container/explore-conta
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [CommonModule, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonSearchbar, IonCol, IonRow, IonGrid, IonImg, IonItem, IonButton, IonLabel, IonThumbnail, IonSkeletonText, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCardSubtitle]
 })
 export class HomePage implements OnInit {
+  public loaded = false;
 
+  jobs_categories = [
+    {
+      id: 'j1',
+      job_name: 'Plumbing',
+      image: 'https://cdn.icon-icons.com/icons2/94/PNG/512/plumbing_16820.png'
+    },
+    {
+      id: 'j2',
+      job_name: 'Repairing',
+      image: 'https://cdn.icon-icons.com/icons2/3045/PNG/512/toolbox_repair_box_tool_box_toolboxes_toolkit_icon_189326.png'
+    },
+    {
+      id: 'j3',
+      job_name: 'Electrical',
+      image: 'https://cdn.icon-icons.com/icons2/3087/PNG/512/generator_electricity_electric_electrical_energy_icon_191371.png'
+    },
+    {
+      id: 'j4',
+      job_name: 'Cleaning',
+      image: 'https://cdn.icon-icons.com/icons2/94/PNG/512/cleaning_16813.png'
+    },
+    {
+      id: 'j5',
+      job_name: 'Carpentry',
+      image: 'https://cdn.icon-icons.com/icons2/94/PNG/512/carpentry_16805.png'
+    },
+    {
+      id: 'j6',
+      job_name: 'Painting',
+      image: 'https://cdn.icon-icons.com/icons2/1617/PNG/512/3700457-art-artist-artistic-paint-painter-painting-palette_108753.png'
+    },
+    {
+      id: 'j7',
+      job_name: 'Moving',
+      image: 'https://cdn.icon-icons.com/icons2/2313/PNG/512/truck_moving_vehicle_transport_transportation_shipping_icon_142014.png'
+    },
+    {
+      id: 'j8',
+      job_name: 'Handyman',
+      image: 'https://cdn.icon-icons.com/icons2/1670/PNG/512/10514manconstructionworkerlightskintone_110617.png'
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
+
+    if (this.jobs_categories.length > 0) {
+       this.loaded = true;
+    }
   }
 
 }
